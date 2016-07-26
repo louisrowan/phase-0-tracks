fbc74feae3aa0798b444ec17c33bed6c1fbe19f0
Louis Rowan <louis.rowan@icloud.com>
2016-07-25 22:17:10 -0400

    pseudocode puppy class complete

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+# Puppy class
+# Species name: Puppy
+# Characteristics ----------
+# Legs: 4
+# Eyes: 2
+# Tail: 1
+# Skin color: varies
+# Name: varies
+# Attitude: varies
+# Behavior --------
+# Run
+# Bark
+# Sleep
+# Play fetch
+# Chew toys

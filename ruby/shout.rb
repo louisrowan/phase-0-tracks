8c1bf6f4a189f8ab1ca57e61bf8e41bcaa917567
Louis Rowan <louis.rowan@icloud.com>
2016-07-26 19:20:26 -0400

    did release 4 and turned shout into a mixin module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,4 @@
+=begin
 module Shout
 	def self.yell_angrily(words)
 		words + "!!!" + " :("
@@ -10,3 +11,30 @@ end
 
 p Shout.yell_angrily("I'm mad")
 p Shout.yelling_happily ("i'm so happy today")
+=end
+
+module Shout
+	def yell_angrily(words)
+		words + "!!!" + " :("
+	end
+
+	def yelling_happily(words)
+		words.upcase + "!" + " :)"
+	end
+end
+
+class Child
+	include Shout
+end
+
+class Baby
+	include Shout
+end
+
+child = Child.new
+p child.yell_angrily "That's mine"
+p child.yelling_happily "It's my birthday!"
+
+baby = Baby.new
+p baby.yelling_happily "deiowjeijegwpijog"
+p baby.yell_angrily "I'm hungry, where's my bottle?"

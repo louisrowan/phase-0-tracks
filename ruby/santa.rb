ffee2b74e476a57fe9f87353cfa822bd4413d114
Louis Rowan <louis.rowan@icloud.com>
2016-07-25 23:24:11 -0400

    added initialization loop

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -16,6 +16,27 @@ class Santa
 	end
 end
 
+=begin
 test = Santa.new
 test.speak
 test.eat_milk_and_cookies "black and white cookie"
+=end
+
+
+# Initialize santas
+=begin
+santas = []
+puts "Enter gender, ethnicity like this:"
+puts "Female, Japanese"
+puts "Press return when finished"
+puts
+
+response = nil
+until response == ''
+	response = gets.chomp
+	response_split = response.split(',')
+	santas << Santa.new(response_split[0], response_split[1])
+end
+
+puts santas
+=end

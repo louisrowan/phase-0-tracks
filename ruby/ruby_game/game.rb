88c77e9abc1f0813696279095d93ebb49981f7c5
Louis Rowan <louis.rowan@icloud.com>
2016-07-28 13:05:44 -0400

    created game files

---
+++ ruby/ruby_game/game.rb
@@ -0,0 +1,13 @@
+=begin
+	
+Input: word or phrase to be guessed
+
+Guess method:
+-- input is a character to be guessed
+-- guess method checks to see if that character is in the word to be guessed
+
+Hint method:
+-- input is the last character guessed
+-- outputs a hint showing only the previously guessed characters
+
+=end

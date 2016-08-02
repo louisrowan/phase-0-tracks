1beef89344cfb6897b6de236c180e2c850e62664
Louis Rowan <louis.rowan@icloud.com>
2016-08-01 20:11:07 -0400

    added pseudocde for reverse function

---
+++ js/explore.js
@@ -0,0 +1,9 @@
+// Pseudocode a function that takes a string as a parameter and reverses the string
+// Step 1: Input a string into the function as a parameter
+// Step 2: Iterate through the string starting with the last character, moving forward
+// Step 3: Add each character to a new string
+// Step 4: Print out the new string. This should be the original string, but reversed.
+
+function reverseString(string) {
+	
+}

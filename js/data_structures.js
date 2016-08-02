2e71995c1406d8487afb51369801ee58df050827
Louis Rowan <louis.rowan@icloud.com>
2016-08-02 14:54:21 -0400

    formatting improvements

--- js/data_structures.js
+++ js/data_structures.js
@@ -16,3 +16,24 @@ for (var i = 0; i < colors.length; i++){
 
 console.log("")
 console.log(horses);
+
+
+// Release 2:
+
+function Car(make, year, runsWell) {
+	this.make = make;
+  this.year = year;
+  this.runsWell = runsWell;
+  this.starts = function() { console.log("*Revs Engine*");};
+  console.log("Car initialization complete");
+}
+
+var newCar = new Car("Toyota", 2012, true);
+console.log(newCar);
+newCar.starts();
+
+var anotherCar = new Car("Mazda", 1990, true);
+console.log(anotherCar);
+anotherCar.starts();
+
+// 

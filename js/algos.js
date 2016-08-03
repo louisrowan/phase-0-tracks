b2ca3d3e39274fdbc566763821596d41996622a0
Louis Rowan <louis.rowan@icloud.com>
2016-08-03 18:27:00 -0400

    reformatted to make it very easy to read

--- js/algos.js
+++ js/algos.js
@@ -1,5 +1,4 @@
-array = ["long phrase", "longest phrase", "longer phrase", "longest phrase ever", "short"]
-
+// Release 0: Find longest phrase
 function longestItem(array) {
 	var longest = ''
 		for ( var i = 0; i < array.length; i++) {
@@ -9,27 +8,7 @@ function longestItem(array) {
 	return longest
 }
 
-
-// console.log(longestItem(array))
-// console.log(longestItem(["longer", 'short']))
-// console.log(longestItem(['bananas', 'wine', 'abcdefghijklmnop', '22', 22]))
-
-
-my_object = {
-	name: 'michael',
-	age: 21,
-	isAPerson: true,
-};
-
-second_object = {
-	name: 'frank',
-	age: 18,
-	isAPerson: true,
-}
-
-
-// Release 1: Create function that checks if 2 objects have a matching key/value pair
-
+// Release 1: Find matching key/value pairs
 function matchingPair(object1, object2) {
 	for (var key in object1)
 		for (var second_key in object2) {
@@ -39,23 +18,7 @@ function matchingPair(object1, object2) {
 	return return_val;
 }
 
-console.log(matchingPair(my_object, second_object)); // -> true
-
-third_object = {
-	location: 'USA',
-	year: 1900
-}
-
-fourth_object = {
-	location: 'China',
-	year: 2000
-}
-
-console.log(matchingPair(third_object, fourth_object)); // -> false
-
-
-// Release 2:
-
+// Release 2: Build an array of N strings with 1-10 random characters
 function buildArray(integer) {
 	array = []
 	letters = 'abcdefghijklmnopqrstuvwxyz'
@@ -69,13 +32,58 @@ function buildArray(integer) {
 	return array
 }
 
-// buildArray(5)
-// console.log(buildArray(4)) // -> [ 'tehsbg', 'wg', 'gfezy', 'iros' ]
-// console.log(buildArray(8)) // -> [ 'it', 'cuj', 'krj', 'olbdea', 'saswpxq', 'dn', 'vzf', 'pyjgqkp' ]
-// console.log(buildArray(3)) // -> [ 'ohcbr', 'dmopp', 'ytpv' ]
+
+// Data Structures: 
+
+array = ["long phrase", "longest phrase", "longer phrase", "longest phrase ever", "short"]
+
+my_object = {
+	name: 'michael',
+	age: 21,
+	isAPerson: true,
+};
+
+second_object = {
+	name: 'frank',
+	age: 18,
+	isAPerson: true,
+}
+
+third_object = {
+	location: 'USA',
+	year: 1900
+}
+
+fourth_object = {
+	location: 'China',
+	year: 2000
+}
+
+// Driver Code for Release 0: 
+console.log('Release 0:')
+console.log(longestItem(array))
+console.log(longestItem(["longer", 'short']))
+console.log(longestItem(['bananas', 'wine', 'abcdefghijklmnop', '22', 22]))
+
+
+
+// Driver Code for Release 1:
+console.log('')
+console.log('Release 1:')
+console.log(matchingPair(my_object, second_object)); // -> true
+console.log(matchingPair(third_object, fourth_object)); // -> false
+
+
+// Driver Code for Release 2:
+console.log('')
+console.log('Release 2:')
+console.log(buildArray(4)) // -> [ 'tehsbg', 'wg', 'gfezy', 'iros' ]
+console.log(buildArray(8)) // -> [ 'it', 'cuj', 'krj', 'olbdea', 'saswpxq', 'dn', 'vzf', 'pyjgqkp' ]
+console.log(buildArray(3)) // -> [ 'ohcbr', 'dmopp', 'ytpv' ]
+console.log('')
+console.log('Part 2:')
 
 // Part 2:
-
 for (var i = 0; i < 10; i++) {
 	var randInteger = buildArray(Math.floor(Math.random()*10)+1)
 	console.log("Array # " + (i + 1) + " = ", randInteger)

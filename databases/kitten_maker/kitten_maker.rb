ba148be3023344e974f2f740e111b53c9c72ccf9
Louis Rowan <louis.rowan@icloud.com>
2016-08-10 12:20:24 -0400

    messing around with kitten_maker.rb

--- databases/kitten_maker/kitten_maker.rb
+++ databases/kitten_maker/kitten_maker.rb
@@ -28,17 +28,37 @@ db.execute(create_table_cmd)
 # add LOOOOTS of kittens!
 # so. many. kittens. 
 #KittenExplosion
-def create_kitten(db, name, age)
-  db.execute("INSERT INTO kittens (name, age) VALUES (?, ?)", [name, age])
+def create_kitten(db, name, address, age)
+  db.execute("INSERT INTO kittens (name, address, age) VALUES (?, ?, ?)", [name, address, age])
 end
 
-10000.times do
-  create_kitten(db, Faker::Name.name, 0)
+def delete_kitten(db)
+	db.execute('delete from kittens where address = 0')
+end
+
+def age_one_year(db)
+	db.execute('update kittens set age=(age + 1)')
+end
+
+age_one_year(db)
+
+delete_kitten(db)
+
+# db.execute('alter table kittens add column address VARCHAR(255)')
+
+
+
+counter = 0
+50.times do
+  create_kitten(db, Faker::Name.name, Faker::Address.street_address, 0)
+  puts counter
+  counter += 1
 end
 
 # explore ORM by retrieving data
-# kittens = db.execute("SELECT * FROM kittens")
-# kittens.each do |kitten|
-#  puts "#{kitten['name']} is #{kitten['age']}"
-# end
+kittens = db.execute("SELECT * FROM kittens")
+kittens.each do |kitten|
+ puts "#{kitten['name']} is #{kitten['age']} and they live at #{kitten['address']}"
+end
+
 

3a56e8c3b5d321853fc0c3f0f29abe2ef53ca37c
Louis Rowan <louis.rowan@icloud.com>
2016-08-10 19:06:30 -0400

    began pseudocoding for game

---
+++ databases/ruby_sql_project/game_class.rb
@@ -0,0 +1,23 @@
+# Game ideas:
+# Purpose: Run a street operation selling illicit goods
+# types of goods:
+# 1. bootleg CDs
+# 2. bootleg movies
+# 3. Knockoff clothing
+# 4. Stolen TVs
+# 5. Stolen computers
+
+# types of actions that can be taken:
+# 1. Hiring
+# people to hire:
+# a. Salespeople - sell goods
+# b. Thiefs - obtain goods
+# c. Crooked cop - helps keep from getting caught
+
+# 2. Increase territory in the city
+# when trying to increase territory:
+# 	a. chance of gaining territory which leads to more Sales
+# 	b. chance of getting caught and staff being arrested
+
+
+

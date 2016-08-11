d419fd18a4c3a482e72d75a722fcf521aafb8c58
Louis Rowan <louis.rowan@icloud.com>
2016-08-10 20:49:05 -0400

    added some instance methods like hire_seller and hire_thief

--- databases/ruby_sql_project/game_class.rb
+++ databases/ruby_sql_project/game_class.rb
@@ -17,36 +17,71 @@
 # 2. Increase territory in the city
 # when trying to increase territory:
 # 	a. chance of gaining territory which leads to more Sales
-# 	b. chance of getting caught and staff being arrested
+# # 	b. chance of getting caught and staff being arrested
+
+# 3. advance one day
 
 
 class Game
 
 	def initialize
 		@cash = 100
-		@sellers = 0
-		@thiefs = 0
+		@sellers = 1
+		@thiefs = 1
 		@police = 0
 		@cds = 5
+		@day = 1
 	end
 
 
-	def make_sale(item, amount)
-		if item == 'cds'
-			@cds -= amount
-			@cash += (amount)*5
-		end
+	def daily_sales
+		daily_sellers = @sellers
+		daily_profit = daily_sellers*20
+		@cds -= daily_sellers
+		@cash += daily_profit
 	end
 
+	def daily_thievery
+		@cds += (@thiefs)*3
+	end
+
+
+	def advance_day
+		@day += 1
+		pay_workers
+		daily_sales
+		daily_thievery
+		status
+	end
+
+	def pay_workers
+		@cash -= (@sellers + @thiefs)*10
+	end
+
+	def hire_seller
+		@sellers += 1
+	end
+
+	def hire_thief
+		@thiefs += 1
+	end
 
 
 	def status
-		puts "#{@cash} cash"
+		puts "Day #{@day}: #{@cash} cash, #{@cds} cds, #{@sellers} sellers."
 	end
 
 end
 
 active_game = Game.new
-active_game.status
-active_game.make_sale('cds', 2)
-active_game.status
+active_game.advance_day
+active_game.advance_day
+active_game.advance_day
+active_game.hire_seller
+active_game.hire_thief
+active_game.hire_seller
+active_game.advance_day
+active_game.advance_day
+active_game.advance_day
+active_game.advance_day
+active_game.advance_day

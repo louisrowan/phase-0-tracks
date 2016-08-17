5eb5469e39dbc7f4cc05a4a8f389f6e154d9b13e
Louis Rowan <louis.rowan@icloud.com>
2016-08-16 21:02:52 -0400

    did jquery assignment

--- web_dev/jquery/jquery.js
+++ web_dev/jquery/jquery.js
@@ -1,9 +1,28 @@
 $(function() {
 
 
+$('.colors').on('click', function(){
+	var color = $(this).attr('id');
+	$('body').css("backgroundColor", color);
+});
 
-$('p').html('bananas');
+$('.animals').on('mouseover', function(){
+	$(this).css("color", "red");
+	var animal = $(this).attr('id');
+	$('#' + animal + 'img').css("opacity", ".5");
+});
 
+$('.animalsclass').on('mouseover', function(){
+	$(this).css("opacity", "1")
+
+});
+
+$('h1').css("color", "blue");
+
+$('#hide').on('click', function(){
+	$('.animalsclass').css("opacity", "0")
+	$('.animals').css("color", "black")
+})
 
 
 });
